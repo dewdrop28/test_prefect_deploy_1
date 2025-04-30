a853fec75536ef768aa7d15218084a1adaa01bf0
dewdrop28 <dewdrop20008@gmail.com>
2025-04-30 23:10:12 +0530

    updating the flow code

--- flows/myflow.py
+++ flows/myflow.py
@@ -3,7 +3,7 @@ from prefect import flow, task, get_run_logger
 
 @flow(name="My Flow", description="My flow description", log_prints=True) 
 def my_flow():
-    get_run_logger().info("Hello and welcome to my flow deployed from GitHub!!!")
+    get_run_logger().info("Hello and welcome to my flow deployed from GitHub")
 
 if __name__ == "__main__":
     my_flow()
