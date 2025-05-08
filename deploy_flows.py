a474a7a8b7bcac5bf9aca852771f877cd0304f4e
dewdrop28 <dewdrop20008@gmail.com>
2025-05-08 21:50:29 +0530

    Deploying my flow...

--- deploy_flows.py
+++ deploy_flows.py
@@ -2,17 +2,20 @@ import os
 from pathlib import Path
 from importlib import import_module
 from prefect.logging import get_logger
+from prefect_github import GitHubCredentials
+from prefect.runner.storage import GitRepository
 
 # Configuration
 FLOWS_DIR = "flows"
 WORK_POOL_NAME = "DewDrop_Pool"
-FLOW_FUNCTION_NAME = "flow_start"
+FLOW_FUNCTION_NAME = "flow_start" # Assumes each file defines a flow named `flow_start`
+GITHUB_TOKEN = "github-token"
 
 logger = get_logger()
 
 def deploy_all_flows():
     flow_files = Path(FLOWS_DIR).glob("*.py")
-
+    
     for flow_file in flow_files:
         module_path = flow_file.with_suffix("")  # Remove .py
         module_name = f"{FLOWS_DIR}.{module_path.name}"
@@ -22,17 +25,24 @@ def deploy_all_flows():
             flow_fn = getattr(mod, FLOW_FUNCTION_NAME)
 
             logger.info(f"Deploying flow from {module_name}.{FLOW_FUNCTION_NAME}...")
-
-            deployment = flow_fn.deploy(
+            
+            github_credentials = GitHubCredentials(token=GITHUB_TOKEN)
+            
+            logger.info(f"check: ./{FLOWS_DIR}/{module_path.name}.py:{FLOW_FUNCTION_NAME}")
+            deployment = flow_fn.from_source(source=source, entrypoint=f"./{FLOWS_DIR}/{module_path.name}.py:{FLOW_FUNCTION_NAME}").deploy(
                 name=f"{module_path.name}-deployment",
                 work_pool_name=WORK_POOL_NAME,
                 tags=["ci", module_path.name]
             )
 
-            deployment.apply()
-
+            logger.info(f"✅ Deployed: {module_path.name}-deployment")
         except Exception as e:
-            logger.error(f"Failed to deploy flow from {module_name}: {e}")
+            logger.error(f"❌ Failed to deploy flow from {module_name}: {e}")
 
 if __name__ == "__main__":
+    source = GitRepository(
+        url="https://github.com/dewdrop28/test_prefect_deploy_1.git",
+        credentials=GitHubCredentials.load(GITHUB_TOKEN)
+    )
+
     deploy_all_flows()
---
+++ flows/yourflow.py
@@ -0,0 +1,9 @@
+from prefect import flow, task, get_run_logger
+
+@flow(name="Your Flow", description="Your flow description", log_prints=True) 
+def flow_start():
+    get_run_logger().info("Hello and welcome to your flow deployed from GitHub.")
+    get_run_logger().info("Hello again in your flow...")
+
+if __name__ == "__main__":
+    my_flow()
