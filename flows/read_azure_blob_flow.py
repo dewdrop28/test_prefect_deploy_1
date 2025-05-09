b02a19d47cc48ae5d853a1d0cb27607a25dcf437
dewdrop28 <dewdrop20008@gmail.com>
2025-05-09 20:35:16 +0530

    Adding flow to read Azure BLOB

--- flows/read_azure_blob_flow.py
+++ flows/read_azure_blob_flow.py
@@ -1,22 +1,11 @@
 from prefect import flow, task, get_run_logger
-from prefect_azure import AzureBlobStorageCredentials
 from azure.storage.blob import BlobServiceClient
 from prefect.blocks.system import Secret
 
 @task
-def read_blob():
-    # Specify the container and blob names
-    container_name = "container1"
-    blob_name = "source_dir/source1.csv"
-        
+def read_blob(container_name: str, blob_name: str):    
     logger = get_run_logger()
     
-    # Load the Azure Blob Storage credentials block
-    #credentials = AzureBlobStorageCredentials.load("azure-blob-credentials")
-
-    # Create a BlobServiceClient using the credentials
-    #blob_service_client = credentials.get_client()
-
     # Load the connection string from a Prefect Secret block
     conn_str = Secret.load("azure-conn-string").get()
     blob_service_client = BlobServiceClient.from_connection_string(conn_str)
@@ -34,7 +23,7 @@ def read_blob():
 
 @flow
 def azure_blob_flow():
-    read_blob()
+    read_blob(container_name="container1", blob_name="source_dir/source1.csv")
 
 if __name__ == "__main__":
     azure_blob_flow()
